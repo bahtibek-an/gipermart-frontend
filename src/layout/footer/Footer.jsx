62965a21f424d78422798b8cc7730a9abb8809d1
bahtibek-an <bahtibekanvarov@gmail.com>
2023-02-24 22:29:18 +0500

    add numbers soc media in footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,14 +1,18 @@
 import { Container } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../../assets/scss/_footer.scss";
 import { AiFillFacebook, AiOutlineArrowUp } from "react-icons/ai";
 import { FaInstagramSquare, FaYoutubeSquare } from "react-icons/fa";
 import { FaTelegramPlane } from "react-icons/fa";
 import { Button, IconButton } from "@mui/material";
+import $host from "../../http";
 
 const Footer = () => {
   const [scrollTopBtn, setScrollTopBtn] = useState(false);
+  const [ socMedia, setSocMedia ] = useState([]);
+  const [ phoneNumbers, setPhoneNumbers ] = useState([]);
+
   const handleWindowScroll = () => {
     if (window.scrollY > 40) {
       setScrollTopBtn(true);
@@ -20,6 +24,24 @@ const Footer = () => {
   const handleScrollTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   };
+
+  const fetchSocMedia = async () => {
+    const { data: { results } }  = await $host.get("outside/site-settings/");
+    setSocMedia(results);
+    return results;
+  }
+
+  const fetchNumbers = async () => {
+    const { data: { results } } = await $host.get("outside/phone-site-settings/")
+    setPhoneNumbers(results);
+    return results;
+  }
+  
+  useEffect(() => {
+    fetchSocMedia();
+    fetchNumbers();
+  }, []);
+
 
   return (
     <footer className="footer">
@@ -33,24 +55,40 @@ const Footer = () => {
                 alt=""
               />
             </Link>
-            <div className="footer__link">
+            {/* <div className="footer__link">
               <a className="text-xl mb-1 block" href="tel:+998900511676">
                 +998900511676
               </a>
               <div className="text-sm text-black">справочная служба</div>
-            </div>
-            <div className="footer__link">
+            </div> */}
+            {/* <div className="footer__link">
               <a className="text-xl mb-1 block" href="tel:+998900511676">
                 +998900511676
               </a>
               <div className="text-sm text-black">интернет-магазин</div>
-            </div>
+            </div> */}
+            {phoneNumbers.length > 0 && phoneNumbers?.map((item) => (
+              <div className="footer__link" key={item.id}>
+                <a className="text-xl mb-1 block" href="tel:+998900511676">
+                  {item.phonenumbers}
+                </a>
+                <div className="text-sm text-black">{item.site_type}</div>
+              </div>
+            ))}
             <div className="footer__link">
               <div className="f-bold text-sm text-black">
                 Оставайтесь на связи
               </div>
               <div className="footer__icons flex items-center gap-4 mt-3">
-                <Link to="/">
+                { socMedia.length > 0 && socMedia?.map((item) => (
+                  <a key={item.id} href={item.link} target="_blank">
+                    <img 
+                      src={item.logo} 
+                      className="h-7 rounded"
+                    />
+                  </a>
+                ))}
+                {/* <Link to="/">
                   <AiFillFacebook fill="#000" size={32} />
                 </Link>
                 <Link to="/">
@@ -61,7 +99,7 @@ const Footer = () => {
                 </Link>
                 <Link to="/">
                   <FaYoutubeSquare fill="#000" size={32} />
-                </Link>
+                </Link> */}
               </div>
             </div>
           </div>
